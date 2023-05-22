29bbe037ae31fb1f2c3af5b1ee4e813926713a27
Stepalom77 <stepalom22@gmail.com>
2023-05-22 13:36:38 -0500

    Agregando los ultimos endpoints

--- employeesController.js
+++ employeesController.js
@@ -130,7 +130,72 @@ const getEmployeeWithPrivileges = async (req, res) => {
   }
   console.log(employees)
   return res.status(200).json(employees)
-}
+};
+
+const createEmployee = async (req, res) => {
+  let employees = [];
+  let newEmployee = null;
+  try {
+      const data = await fs.promises.readFile(employeesFilePath, 'utf8');
+      employees = employees = JSON.parse(data);
+      newEmployee = req.body;
+      employees.push(newEmployee)
+  } catch(error) {
+      console.error(error);
+      if (employees.length === 0) {
+          return res.status(404).json({ message: 'No se encontró ningún empleado' });
+        } else if (!newEmployee) {
+          res.status(400).json({"code": "bad_request"})
+        } else {
+          res.status(400).json({message: 'Huvo un error al tratar de crear el empleado'})
+        }
+  }
+  return res.status(200).json(employees)
+};
+
+const getEmployeeWithBlackBadge = async (req, res) => {
+  let employees = [];
+  const badges = req.query.badges;
+  try {
+      const data = await fs.promises.readFile(employeesFilePath, 'utf8');
+      const jsonData = JSON.parse(data);
+        for(let i in jsonData) {
+          if(jsonData[i].badges.includes(badges)) {
+            employees.push(jsonData[i])
+          }
+        }
+  } catch(error) {
+      if (employees.length === 0) {
+          return res.status(404).json({ message: 'No se encontró ningún empleado' });
+        } else {
+          res.status(400).json({message: 'Huvo un error al tratar de obtener los empleador'})
+        }
+  }
+  console.log(employees)
+  return res.status(200).json(employees)
+};
+
+const getEmployeeWithName = async (req, res) => {
+  let employees = [];
+  const name = req.query.name;
+  try {
+      const data = await fs.promises.readFile(employeesFilePath, 'utf8');
+      const jsonData = JSON.parse(data);
+        for(let i in jsonData) {
+          if(jsonData[i].name === name) {
+            employees.push(jsonData[i])
+          }
+        }
+  } catch(error) {
+      if (employees.length === 0) {
+          return res.status(404).json({ message: 'No se encontró ningún empleado' });
+        } else {
+          res.status(400).json({message: 'Huvo un error al tratar de obtener los empleador'})
+        }
+  }
+  console.log(employees)
+  return res.status(200).json(employees)
+};
 
 module.exports = {
     getAllEmployees,
@@ -138,7 +203,10 @@ module.exports = {
     getEmployeesPageTwo,
     getEmployeesByPage,
     getOldestEmployee,
-    getEmployeeWithPrivileges
+    getEmployeeWithPrivileges,
+    createEmployee,
+    getEmployeeWithBlackBadge,
+    getEmployeeWithName
   };
 
   
--- employeesRoutes.js
+++ employeesRoutes.js
@@ -8,5 +8,8 @@ router.get("/employees/page-two", employees.getEmployeesPageTwo);
 router.get("/employees/page", employees.getEmployeesByPage);
 router.get("/employees/oldest", employees.getOldestEmployee);
 router.get("/employees/privileges", employees.getEmployeeWithPrivileges);
+router.get("/employees/badges", employees.getEmployeeWithBlackBadge);
+router.get("/employees/name", employees.getEmployeeWithName);
+router.post("/employees", employees.createEmployee);
 
 module.exports = router;
